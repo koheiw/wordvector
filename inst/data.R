72cd931eab1362aac264083070f420af36780037
Kohei Watanabe <watanabe.kohei@gmail.com>
2024-12-07 10:21:19 +0900

    Clean the texts

--- inst/data.R
+++ inst/data.R
@@ -1,11 +1,37 @@
 library(quanteda)
+library(stringi)
 
 dat <- readRDS('~/yahoo-news.RDS')
+dat <- dat[order(dat$time),]
+dat$head <- stri_replace_all_regex(dat$head, "\\s+", " ") %>% 
+    stri_trans_nfkc() %>% 
+    stri_trim()
+dat$body <- stri_replace_all_regex(dat$body, "\\s+", " ") %>% 
+    stri_trans_nfkc() %>% 
+    stri_trim()
+dat$time <- NULL
+
+# # both head and body
 dat$text <- paste0(dat$head, ". ", dat$body)
+dat$head <- NULL
 dat$body <- NULL
-corp <- corpus(dat, text_field = 'text')
+dat <- subset(dat, !duplicated(text))
+corp <- corpus(dat, text_field = "text", docid_field = "tid")
+
+# # only body
+# dat <- subset(dat, !duplicated(body))
+# corp <- corpus(dat, text_field = "body", docid_field = "tid")
+
+# set.seed(1234)
+# data_corpus_news <- corpus_sample(corp, 20000)
+# usethis::use_data(data_corpus_news)
+# 
+# set.seed(1234)
+# data_corpus_news2015 <- corpus_subset(corp, lubridate::year(date) == 2015) %>%
+#     corpus_sample(20000)
+# usethis::use_data(data_corpus_news2015)
 
 set.seed(1234)
-data_corpus_news2014 <- corpus_sample(corp, 20000)
-
+data_corpus_news2014 <- corpus_subset(corp, lubridate::year(date) == 2014) %>%
+    corpus_sample(20000)
 usethis::use_data(data_corpus_news2014)
