9553cf90d22217cd2a4968f23ed5b81885b2e790
Kohei Watanabe <watanabe.kohei@gmail.com>
2024-12-07 11:03:53 +0900

    Clean up tests

--- tests/misc/test_large.R
+++ tests/misc/test_large.R
@@ -1,57 +1,28 @@
 library(quanteda)
-library(wordvector)
-library(LSX)
 library(word2vec)
+library(wordvector)
+library(quanteda.textstats)
+options(wordvector_threads = 8)
 
-data_corpus_guardian <- readRDS("C:/Users/watan/Dropbox/Public/data_corpus_guardian2016-10k.rds")
-corp <- data_corpus_guardian %>% 
-    corpus_reshape()
+# Load data
+dat <- readRDS('~/yahoo-news.RDS')
+dat$text <- paste0(dat$head, ". ", dat$body)
+corp <- corpus(dat, text_field = 'text', docid_field = "tid")
 
+# Pre-processing
 toks <- tokens(corp, remove_punct = TRUE, remove_symbols = TRUE) %>% 
-    tokens_remove(stopwords(), padding = TRUE) %>% 
+    tokens_remove(stopwords("en", "marimo"), padding = TRUE) %>% 
     tokens_select("^[a-zA-Z-]+$", valuetype = "regex", case_insensitive = FALSE,
                   padding = TRUE) %>% 
     tokens_tolower()
-lis <- as.list(toks)
 
-#options(wordvector_threads = 0)
-wdv <- wordvector::word2vec(toks, dim = 50, iter = 5, min_count = 5, verbose = TRUE, sample = 0.001) #type = "skip-gram")
-lsa <- wordvector::lsa(toks, dim = 300, min_count = 5, verbose = TRUE)
-lsa2 <- wordvector::lsa(toks, dim = 100, min_count = 5, verbose = TRUE, weight = "sqrt")
-lsa3 <- wordvector::lsa(toks, dim = 100, min_count = 5, verbose = TRUE, reduce = TRUE)
-w2v <- word2vec::word2vec(lis, dim = 50, iter = 5, min_count = 5, #type = "skip-gram",
-                          threads = 6)
+wdv <- word2vec(toks, dim = 50, type = "cbow", min_count = 5, verbose = TRUE)
 
-analogy(wdv, ~ australia)
-analogy(lsa, ~ australia)
-analogy(lsa2, ~ australia)
-analogy(lsa3, ~ australia)
-analogy(lsa3, ~ australia -animal)
-analogy(lsa3, ~ amazon - commerce)
-analogy(lsa3, ~ amazon - forest)
-analogy(wdv, ~ australia - animal)
-analogy(wdv, ~ amazon - commerce)
-analogy(wdv, ~ amazon - forest - computer)
-
-emb <- as.matrix(w2v)
-vec <- emb["amazon",] - emb["commerce",]
-#vec <- emb["japan",]# - emb["china",]
-#vec <- emb["king",] - emb["queen",]
-predict(w2v, vec, type = "nearest", top_n = 10)
-
-predict(w2v, c("bus", "toilet"), type = "nearest", top_n = 10)
-
-#synonyms(wdv, c("good", "bad", "new", "america", "japan", "bus", "toilet"))
-synonyms(wdv, c("bus", "toilet"))
-synonyms(wdv, c("amazon", "commerce"))
-head(model.frame(~ japan - tokyo, as.data.frame.matrix(t(emb))))
-
-
-lsa <- lsa(toks, dim = 200, verbose = TRUE)
-synonyms(lsa, c("amazon", "commerce"))
-analogy(lsa, ~ amazon - forest - computer)
-analogy(lsa, ~ forest)
-doc2vec(lsa)
-
-dov <- doc2vec(tokens_group(toks), lsa)
-plot(hclust(dist(as.matrix(dov))))
+analogy(wdv, ~ france + terror, exclude = FALSE, n = 10)
+analogy(wdv, ~ britain, exclude = FALSE, n = 20)
+analogy(wdv, ~ america, exclude = FALSE, n = 20)
+analogy(wdv, ~ america - obama, exclude = FALSE, n = 10)
+analogy(wdv, ~ america - trump, exclude = FALSE, n = 10)
+analogy(wdv, ~ eu - immigrants, exclude = FALSE, n = 10)
+analogy(wdv, ~ eu - refugees, exclude = FALSE, n = 10)
+analogy(wdv, ~ eu - terrorism, exclude = FALSE, n = 10)
--- tests/misc/test_small.R
+++ tests/misc/test_small.R
@@ -1,48 +1,32 @@
 library(quanteda)
 library(wordvector)
-library(LSX)
 library(word2vec)
+#library(quanteda.textstats)
+options(wordvector_threads = 8)
 
-#data_corpus_guardian <- readRDS("/home/kohei/Dropbox/Public/data_corpus_guardian2016-10k.rds")
-#corp <- data_corpus_guardian %>% 
-corp <- data_corpus_inaugural %>% 
-    corpus_reshape()
+corp <- data_corpus_news2014
+toks <- tokens(corp, remove_punct = TRUE, remove_symbols = TRUE) %>%
+   tokens_remove(stopwords("en", "marimo"), padding = TRUE) %>%
+   tokens_select("^[a-zA-Z-]+$", valuetype = "regex", case_insensitive = FALSE,
+                 padding = TRUE) %>%
+   tokens_tolower()
 
-toks <- tokens(corp, remove_punct = TRUE, remove_symbols = TRUE) %>% 
-    tokens_remove(stopwords(), padding = TRUE) %>% 
-    tokens_tolower()
-toks_grp <- tokens_group(toks)
-ndoc(toks)
+# word2vec -------------------------------------
+wdv <- wordvector::word2vec(toks, dim = 50, type = "cbow", min_count = 5, iter = 5, 
+                            verbose = TRUE)
 
-set.seed(1234)
-wov <- wordvector::word2vec(toks, dim = 50, iter = 5, min_count = 5, type = "skip-gram",
-                            verbose = TRUE, threads = 1)
-synonyms(wov, c("fellow-citizens", "country"))
+analogy(wdv, ~ washington - america + france)
+analogy(wdv, ~ berlin - germany + france, exclude = FALSE, n = 10)
 
-dov <- doc2vec(toks_grp, wov)
-dov[1,]
+# Word2vec (old) -------------------------------------
 
-# -------------------------
+lis <- as.list(toks)
+w2v <- word2vec::word2vec(lis, dim = 50, min_count = 0, type = "cbow", iter = 5, threads = 8)
+emb <- as.matrix(w2v)
+vec <- emb["berlin",] - emb["germany",] + emb["france",]
+predict(w2v, vec, type = "nearest", top_n = 10)
 
-set.seed(1234)
-lis0 <- as.list(toks)
-wov0 <- word2vec::word2vec(lis0, 
-                           dim = 50, iter = 5, min_count = 5, type = "skip-gram",
-                           verbose = TRUE, threads = 1)
+# LSA -------------------------------------
 
-synonyms(wov0, c("fellow-citizens", "country"))
-
-lis_grp0 <- as.list(toks_grp)
-dov0 <- word2vec::doc2vec(wov0, newdata = sapply(lis_grp0, paste, collapse = " "))
-dov0[1,]
-
-# ----------------------------------------------
-
-dist0 <- proxyC::dist(dov0)
-plot(hclust(as.dist(dist0)))
-
-dist <- proxyC::dist(dov)
-plot(hclust(as.dist(dist)))
-hist(as.matrix(dov))
-
-cor(dist[,1], dist0[,1])
+lsa <- wordvector::lsa(toks, dim = 50, min_count = 0, verbose = TRUE)
+analogy(wdv, ~ berlin - germany + france, exclude = FALSE, n = 10)
