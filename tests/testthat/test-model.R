ece13137ae05a810b98812bea320f78ca63b2c0c
Kohei Watanabe <watanabe.kohei@gmail.com>
2025-05-12 18:02:53 +0900

    Save test

--- tests/testthat/test-model.R
+++ tests/testthat/test-model.R
@@ -1,42 +1,61 @@
 require(quanteda)
 require(wordvector)
+options(wordvector_threads = 5)
 
 toks0 <- tokens("a b c d e")
 toks <- tokens("a b c d e f")
 
+set.seed(1234)
 wov0 <- textmodel_word2vec(toks, dim = 3, type = "cbow", min_count = 0, #sample = 1,
                            normalize = FALSE)
-wov <- textmodel_word2vec(toks, dim = 3, type = "cbow", min_count = 0, #sample = 1, 
+
+set.seed(1234)
+wov1 <- textmodel_word2vec(toks, dim = 3, type = "cbow", min_count = 0, #sample = 1, 
+                          normalize = FALSE)
+
+set.seed(1234)
+wov2 <- textmodel_word2vec(toks, dim = 3, type = "cbow", min_count = 0, #sample = 1, 
                           model = wov0, 
                           normalize = FALSE)
-#wov0$values
-#wov$values
-f <- intersect(rownames(wov0$values), rownames(wov$values))
-f
-sim <- proxyC::simil(wov0$values[f,], wov$values[f,], diag = TRUE)
-hist(sim@x, xlim = c(-1, 1))
+
+f <- intersect(rownames(wov0$values), rownames(wov1$values))
+
+par(mfrow = c(2, 1), mar = c(3, 3, 3, 2))
+hist(wov0$values[f,] - wov1$values[f,], xlim = c(-1, 1), main = "wov1")
+hist(wov0$values[f,] - wov2$values[f,], xlim = c(-1, 1), main = "wov2")
+par(mfrow = c(1, 1))
 
 # --------------------------------
-
 
 corp <- data_corpus_inaugural %>%
     corpus_reshape()
 toks <- tokens(corp, remove_punct = TRUE, remove_symbols = TRUE) %>%
      tokens_remove(stopwords(), padding = FALSE)
-toks0 <- head(toks, 5000)
-toks <- tail(toks, 1000)
+toks0 <- head(toks, 3000)
+toks1 <- toks2 <- tail(toks, 3000)
 
-wov0 <- textmodel_word2vec(toks, dim = 50, type = "cbow", 
+#set.seed(1234)
+wov0 <- textmodel_word2vec(toks0, dim = 50, type = "cbow", min_count = 5, sample = 1,
                            normalize = FALSE)
-wov <- textmodel_word2vec(toks, dim = 50, type = "cbow", 
-                          model = wov0, iter = 1,
-                          normalize = FALSE)
 
-head(probability(wov0, "america"))
-head(probability(wov, "america"))
+#set.seed(1234)
+wov1 <- textmodel_word2vec(toks1, dim = 50, type = "cbow", min_count = 5, sample = 1, 
+                           normalize = FALSE, verbose = TRUE)
 
-f <- intersect(rownames(wov0$values), rownames(wov$values))
+#set.seed(1234)
+wov2 <- textmodel_word2vec(toks2, dim = 50, type = "cbow", min_count = 5, sample = 1, 
+                           model = wov0, normalize = FALSE, verbose = TRUE,)
+
+f <- intersect(rownames(wov0$values), rownames(wov1$values))
 f
-sim <- proxyC::simil(wov0$values[f,], wov$values[f,], diag = TRUE)
-hist(sim@x, xlim = c(-1, 1))
+
+dfmt <- tail(dfm_group(dfm(toks)), 10)
+dov0 <- textmodel_doc2vec(dfmt, wov0)
+dov1 <- textmodel_doc2vec(dfmt, wov1)
+dov2 <- textmodel_doc2vec(dfmt, wov2)
+
+Matrix::diag(proxyC::simil(dov0$values, dov1$values))
+Matrix::diag(proxyC::simil(dov0$values, dov2$values))
+
+
 
