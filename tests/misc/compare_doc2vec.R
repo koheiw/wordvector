8ab0e75ad0b24190a931603a27115f8458573e32
Kohei Watanabe <watanabe.kohei@gmail.com>
2025-09-30 08:33:32 +0900

    Update tests

--- tests/misc/compare_doc2vec.R
+++ tests/misc/compare_doc2vec.R
@@ -14,11 +14,11 @@ rownames(dat3) <- dat3$doc_id
 
 d2v <- paragraph2vec(dat3, dim = 50, threads = 1, type = "PV-DM", trace = FALSE)
 mat_d2v <- as.matrix(d2v, which = "docs", normalize = FALSE)
-hist(mat_d2v["4263794",, drop = TRUE])
+hist(mat_d2v["4362315",, drop = TRUE])
 
 sim_d2v <- proxyC::simil(
     mat_d2v,
-    mat_d2v["4263794",, drop = FALSE]
+    mat_d2v["4362315",, drop = FALSE]
 )
 
 sim_d2v <- proxyC::simil(
@@ -41,15 +41,15 @@ mean(diag(sim_d2v_all)) - mean(sim_d2v_all)
 corp <- corpus(dat3)
 toks <- tokens(corp)
 dfmt <- dfm(toks, remove_padding = TRUE)
-options(wordvector_threads = 1)
+options(wordvector_threads = 8)
 wdv <- textmodel_doc2vec(toks, dim = 50, type = "cbow", min_count = 5, verbose = TRUE, iter = 5,
                          tolower = FALSE, alpha = 0.05)
 mat_wdv <- as.matrix(wdv, layer = "documents", normalize = FALSE)
-hist(mat_wdv["4263794",, drop = TRUE])
+hist(mat_wdv["4362315",, drop = TRUE])
 
 sim_wdv <- proxyC::simil(
     mat_wdv,
-    mat_wdv["4263794",, drop = FALSE]
+    mat_wdv["4362315",, drop = FALSE]
 )
 
 sim_wdv <- proxyC::simil(
@@ -61,11 +61,11 @@ hist(rowSums(sim_wdv))
 tail(sort(s <- rowSums(sim_wdv)))
 tail(dat3[order(s),])
 
-sim_wdv2_all <- proxyC::simil(
+sim_wdv_all <- proxyC::simil(
     mat_wdv,
     mat_wdv[tail(dat3$doc_id, 100),, drop = FALSE]
 )
-mean(diag(sim_wdv2_all)) - mean(sim_wdv2_all)
+mean(diag(sim_wdv_all)) - mean(sim_wdv_all)
 
 # -------------------------
 
