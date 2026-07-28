43355d1c055c5458c3df4cf364e22069b0402546
Kohei Watanabe <watanabe.kohei@gmail.com>
2026-07-28 14:41:03 +0900

    Set threads = 2

--- vignettes/doc2vec.R
+++ vignettes/doc2vec.R
@@ -1,14 +1,13 @@
 ## ----setup, include=FALSE-----------------------------------------------------
 knitr::opts_chunk$set(echo = TRUE, cache = TRUE, collapse = TRUE)
+if (identical(Sys.getenv("NOT_CRAN"), "false"))
+    options(quanteda_threads = 2, wordvector_threads = 2)
 
-## ----eval=FALSE---------------------------------------------------------------
-# # Load data
-# f <- tempfile()
-# download.file('https://www.dropbox.com/s/e19kslwhuu9yc2z/yahoo-news.RDS?dl=1',
-#               f, mode = "wb")
-
-## ----echo=FALSE---------------------------------------------------------------
-f <- "~/yahoo-news.RDS"
+## ----eval=TRUE, cache=TRUE----------------------------------------------------
+# Load data
+f <- tempfile()
+download.file('https://www.dropbox.com/s/e19kslwhuu9yc2z/yahoo-news.RDS?dl=1', 
+              f, mode = "wb")
 
 ## -----------------------------------------------------------------------------
 library(quanteda)
@@ -26,9 +25,9 @@ toks <- tokens(corp, remove_punct = TRUE, remove_symbols = TRUE) %>%
     tokens_select("^[a-zA-Z-]+$", valuetype = "regex", case_insensitive = FALSE,
                   padding = TRUE)
 
-## -----------------------------------------------------------------------------
+## ----cache=TRUE---------------------------------------------------------------
 # Set the number of processors
-options(wordvector_threads = 8)
+options(wordvector_threads = 16)
 
 # Train doc2vec
 dov <- textmodel_doc2vec(toks, dim = 50, type = "dm", min_count = 5, verbose = TRUE)
